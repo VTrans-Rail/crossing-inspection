ac1c6b5bd380465f8d70a26452c2a85c71c213ca
jfarmer91 <farmerjong@gmail.com>
2015-11-24 12:26:07 -0500

    Removed Old InfoTemplate Format Comments

--- search.js
+++ search.js
@@ -112,8 +112,6 @@ require([
     //Create Crossing Feature Layer
     var crossingUrl = "http://services1.arcgis.com/NXmBVyW5TaiCXqFs/arcgis/rest/services/CrossingInspections2015/FeatureServer/1";
 
-    // var crossingTemplate = new InfoTemplate("Railroad Crossing", "DOT Crossing Number: ${DOT_Num}</br>Line Name: ${LineName}</br>Feature Crossed: ${Feature_Crossed}</br>Warning Device Level: ${WDCode}</br>Crossing Codition: ${XingCond}");
-
     var crossingPoints = new FeatureLayer(crossingUrl, {
       id: "crossing-points",
       outFields: ["*"],
@@ -124,8 +122,6 @@ require([
 
     //Create Sign Feature Layer
     var signUrl = "http://services1.arcgis.com/NXmBVyW5TaiCXqFs/arcgis/rest/services/CrossingInspections2015/FeatureServer/0";
-
-    // var signTemplate = new InfoTemplate("Crossing Sign", "DOT Crossing Number: ${DOT_Num}</br>Sign Type: ${SignType}</br>Sign Condition: ${SignCondition}</br>Installation Date: ${InstallDate}");
 
     var signPoints = new FeatureLayer(signUrl, {
       id: "sign-points",
@@ -138,8 +134,6 @@ require([
     //Create Rail Line Feature Layer
     var lineUrl = "http://vtransmap01.aot.state.vt.us/arcgis/rest/services/Rail/Rail_Lines/MapServer/0";
 
-    // var lineTemplate = new InfoTemplate("Railroad Line", "Line Name: ${LineName}</br>Division: ${Division}</br>Subdivision: ${Subdivision}</br>Branch: ${Branch}");
-
     var railLine = new FeatureLayer(lineUrl, {
       id: "rail-line",
       outFields: ["*"],
@@ -149,8 +143,6 @@ require([
 
     //Create AADT Line Feature Layer
     var aadtUrl = "https://services1.arcgis.com/NXmBVyW5TaiCXqFs/ArcGIS/rest/services/AADT_2013_StateHighways/FeatureServer/0";
-
-    // var aadtTemplate = new InfoTemplate("Average Annual Daily Traffic", "AADT Count: ${aadt}</br>Year: ${YEAR}</br>Station ID: ${ATRStation}");
 
     var aadtLine = new FeatureLayer(aadtUrl, {
       id: "aadt-line",
@@ -192,10 +184,9 @@ require([
       maxResults: 15,
       maxSuggestions: 15,
 
-
-      //Create an InfoTemplate and include three fields
+      //Create an InfoTemplate
       infoTemplate: crossingTemplate,
-      // new InfoTemplate("Railroad Crossing", "DOT Crossing Number: ${DOT_Num}</br>Line Name: ${LineName}</br>Feature Crossed: ${Feature_Crossed}</br>Warning Device Level: ${WDCode}</br>Crossing Codition: ${XingCond}"),
+
       enableSuggestions: true,
       minCharacters: 0
     });
@@ -216,7 +207,6 @@ require([
 
       //Create an InfoTemplate
       infoTemplate: signTemplate,
-      // new InfoTemplate("Crossing Sign Information", "DOT # of Associated Crossing: ${DOT_Num}</br>Type of Sign: ${SignType}</br>Condition: ${SignCondition}"),
 
       enableSuggestions: true,
       minCharacters: 0
@@ -231,7 +221,6 @@ require([
     //Set the countryCode for World Geocoding Service
     searchWidget.sources[2].countryCode = "US";
     searchWidget.sources[2].maxSuggestions = 4;
-
 
     //Finalize creation of the search widget
     searchWidget.startup();
