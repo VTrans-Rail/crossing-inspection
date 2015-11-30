ab91ccb902b9fd1585cf796f8090ee1002d95887
jfarmer91 <farmerjong@gmail.com>
2015-11-30 10:39:44 -0500

    Added Link to Report Page on Popup (Unfinished): Part of Issue #25
    Added link to popup that takes you to the report page. It does not pass
    along the selection yet. Styling changed because I had to use the
    description part of the PopupTemplate to add field data and hyperlink
    instead of the fieldInfos section which comes with theme styling to
    highlight the data from the field name. Should figure out how to remedy
    this later

--- search.js
+++ search.js
@@ -54,20 +54,27 @@ require([
 
 // -----------------Define PopupTemplates------------------------------
     //Crossing Template
-    var crossingTemplate = new PopupTemplate({
-      title: "Summary Info for Crossing {DOT_Num}",
 
-      fieldInfos: [
-        { fieldName: "DOT_Num", label: "DOT Crossing Number", visible: true, format: { places: 0} },
-        { fieldName: "LineName", label: "Rail Line", visible: true, format: { places: 0} },
-        { fieldName: "Feature_Crossed", label: "Feature Crossed", visible: true, format: { places: 0} },
-        { fieldName: "WDCode", label: "Warning Device Level", visible: true, format: { places: 0} },
-        { fieldName: "SurfaceType", label: "Primary Crossing Surface Material", visible: true, format: { places: 0} },
-        { fieldName: "XingCond", label: "Overall Condition", visible: true, format: { places: 0} },
-      ],
+    var crossingPopupFeatures = "DOT Crossing Number: {DOT_Num}</br>Line Name: {LineName}</br>Feature Crossed: {Feature_Crossed}</br>Warning Device Level: {WDCode}</br>Primary Crossing Surface Material: {SurfaceType}</br>Crossing Codition: {XingCond}";
+
+    var crossingTemplate = new PopupTemplate({
+      title: "Railroad Crossing {DOT_Num}",
+
+      description: crossingPopupFeatures + "</br></br><a href='ReportTest.html'>Full Report</a>",
+
+      // fieldInfos: [
+      //   { fieldName: "DOT_Num", label: "DOT Crossing Number", visible: true, format: { places: 0} },
+      //   { fieldName: "LineName", label: "Rail Line", visible: true, format: { places: 0} },
+      //   { fieldName: "Feature_Crossed", label: "Feature Crossed", visible: true, format: { places: 0} },
+      //   { fieldName: "WDCode", label: "Warning Device Level", visible: true, format: { places: 0} },
+      //   { fieldName: "SurfaceType", label: "Primary Crossing Surface Material", visible: true, format: { places: 0} },
+      //   { fieldName: "XingCond", label: "Overall Condition", visible: true, format: { places: 0} },
+      // ],
 
       showAttachments: true,
     });
+
+
 
     //Sign Template
     var signTemplate = new PopupTemplate({
