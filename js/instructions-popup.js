e3b7861d8a6e5b5022badca41b5a15ec1b140004
TheMapSmith <stephen@mapsmith.net>
2015-12-08 11:24:36 -0500

    bring new style into instructions js

--- js/instructions-popup.js
+++ js/instructions-popup.js
@@ -4,59 +4,73 @@ function clicker(){
 		thediv.style.display = "";
 		thediv.innerHTML = `
 		<div class="site-wrapper">
-			<div class="site-wrapper-inner">
-				<div class="cover-container">
-					<div class="masthead clearfix">
-						<div class="inner">
-							<h2 class="masthead-brand">Crossing Inspection</h2>
-							<h3 class="subtitle"><em>Data Investigation App</em></h3>
-							<h2 class="subtitle"><small>Here's how it works:</small></h2>
-						</div>
-					</div>
+      <div class="site-wrapper-inner">
+        <div class="cover-container">
+          <div class="masthead clearfix">
+            <div class="inner">
+              <h2 class="masthead-brand">Crossing Inspection</h2>
+              <h3 class="subtitle"><em>Data Investigation App</em></h3>
+              <h3 class="subtitle"><small>Here's how it works:</small></h3>
+            </div>
+          </div>
 
-					<div class="inner cover">
-						<p class="step">
-							<span class="searchIcon esri-icon-search" id="searchico"> </span>
-							<strong>
-								Search for
-							</strong>
-						</p>
-						<p>
-							<ul class="list-inline">
-								<li>DOT Number</li>
-								<li>Town Name</li>
-								<li>Road Name</li>
-							</ul>
-						</p>
-						<div class="horiz-line">
-							<span class="divider-text">
-								<em>or</em>
-							</span>
-						</div>
-						<p class="step">
-							<span class="searchIcon esri-icon-zoom-out-fixed"> </span>
-							<strong>
-								Use the map
-							</strong>
-						</p>
-						<p>
-							<em>
-								Navigate the map to your desired area.
-							</em>
-						</p>
-					</div>
-					<div class="inner cover gotit">
-						<p class="lead">
-							<a href="#" onclick="return clicker();"class="btn btn-lg btn-success">Got it!</a>
-						</p>
-					</div>
+          <div class="inner cover">
+            <p class="step">
+              <span class="searchIcon esri-icon-search" id="searchico"> </span>
+              <strong>
+                Search for
+              </strong>
+            </p>
+            <p>
+              <ul class="list-inline">
+                <li>DOT Number</li>
+                <li>Town Name</li>
+                <li>Road Name</li>
+              </ul>
+            </p>
+            <div class="horiz-line">
+              <span class="divider-text">
+                <em>or</em>
+              </span>
+            </div>
+            <p class="step">
+              <span class="searchIcon esri-icon-zoom-out-fixed"> </span>
+              <strong>
+                Use the map
+              </strong>
+            </p>
+            <p>
+              <em>
+                Navigate the map to your desired area.
+              </em>
+            </p>
+            <div class="horiz-line">
+              <span class="divider-text">
+                <em>then</em>
+              </span>
+            </div>
+
+            <p>
+            <img src="img\crossing-good.png" alt="" id="xingImg"/>
+            <em>
+              Click on a crossing to see details
+            </em>
+            </p>
+            <p>
+              <em>Use the</em><strong> "Full Report" </strong><em>link in the
+              popup for more details.</em>
+            </p>
+          </div>
+          <div class="inner cover gotit">
+            <p class="lead">
+              <a href="#" onclick="return clicker();"class="btn btn-lg btn-success">Got it!</a>
+            </p>
+          </div>
 
 
-				</div>
+        </div>
 
-			</div>
-
-		</div>
+      </div>
 		`;
 	}else{
 		thediv.style.display = "none";
