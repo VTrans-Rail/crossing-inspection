74c68cd7392fd077ae5f1951f29336429a8b7e99
jfarmer91 <farmerjong@gmail.com>
2015-12-10 11:19:09 -0500

    Part of Issue #162: Added World Transportation Reference Layer (Dirty)
    - Removed Streets map service and replaced with world imagery.
    - Added World Transportation map service on top of imagery
    (http://services.arcgisonline.com/arcgis/rest/services/Reference/World_Transportation/MapServer)
    - Removed LayerList toggle and BasemapToggle
    
    DIRTY CODE

--- js/main.js
+++ js/main.js
@@ -67,14 +67,21 @@ require([
 //-------------------------------------------------------------
     // satellite imagery from ArcGIS Online, use levels 0 - 11
     var topoBasemap = new   ArcGISTiledMapServiceLayer("http://services.arcgisonline.com/arcgis/rest/services/World_Topo_Map/MapServer", {
-      displayLevels: [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11]
+      displayLevels: [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14]
     });
 
-    // street Map service from ArcGIS Online, use levels 11 - 15
-    var streetBasemap = new ArcGISTiledMapServiceLayer("http://services.arcgisonline.com/arcgis/rest/services/World_Street_Map/MapServer", {
-      displayLevels: [12, 13, 14, 15, 16, 17, 18, 19],
-      opacity : 0.65
+    // satellite Map service from ArcGIS Online, use levels 11 - 15
+    var imageryBasemap = new ArcGISTiledMapServiceLayer("http://services.arcgisonline.com/arcgis/rest/services/World_Imagery/MapServer", {
+      displayLevels: [15, 16, 17, 18, 19],
+      // opacity : 0.65
     });
+
+    // transportation reference layer map service from ArcGIS Online, use levels 11 - 15
+    var streetReferenceLayer = new ArcGISTiledMapServiceLayer("http://services.arcgisonline.com/arcgis/rest/services/Reference/World_Transportation/MapServer", {
+      displayLevels: [15, 16, 17, 18, 19],
+      // opacity : 0.65
+    });
+
 
     // create the map and use the custom zoom levels
     var map = new Map("map", {
@@ -88,7 +95,8 @@ require([
 
     // map.on("extent-change", changeScale);
     map.addLayer(topoBasemap);
-    map.addLayer(streetBasemap);
+    map.addLayer(imageryBasemap);
+    map.addLayer(streetReferenceLayer);
 
     //Resize Popup To Fit titlePane
     map.infoWindow.resize(300, 370)
@@ -96,28 +104,28 @@ require([
 
 
 
-//-------------------------------------------------------------
-//---------------------Create BasemapToggle--------------------
-//-------------------------------------------------------------
-    var toggle = new BasemapToggle({
-      map: map,
-      basemap: "satellite",
-      visible: false,
-    }, "BasemapToggle");
-    toggle.startup();
-
-
-    //Turn on BasemapToggle when zoomed in to specific level
-    map.on("extent-change", checkBasemapToggle);
-    function checkBasemapToggle () {
-      var zoom = map.getZoom();
-      if ( zoom > 11 ) {
-        toggle.show();
-      } else {
-        toggle.hide();
-      }
-    }
-//-------------------------------------------------------------
+// //-------------------------------------------------------------
+// //---------------------Create BasemapToggle--------------------
+// //-------------------------------------------------------------
+//     var toggle = new BasemapToggle({
+//       map: map,
+//       basemap: "satellite",
+//       visible: false,
+//     }, "BasemapToggle");
+//     toggle.startup();
+//
+//
+//     //Turn on BasemapToggle when zoomed in to specific level
+//     map.on("extent-change", checkBasemapToggle);
+//     function checkBasemapToggle () {
+//       var zoom = map.getZoom();
+//       if ( zoom > 11 ) {
+//         toggle.show();
+//       } else {
+//         toggle.hide();
+//       }
+//     }
+// //-------------------------------------------------------------
 
 
 
@@ -154,18 +162,18 @@ require([
     signTemplate.setContent("<b>Oops!</b></br>The summary information and pictures for this sign did not load properly. Please refresh popup window by closing it and clicking on the sign again.");
 
 
-    //AADT Template--------------------
-    var aadtTemplate = new PopupTemplate({
-      title: "Traffic Data",
-
-      fieldInfos: [
-        { fieldName: "aadt", label: "AADT", visible: true, format: { places: 0} },
-        { fieldName: "ATRStation", label: "Automated Traffic Recording Station", visible: true, format: { places: 0} },
-        { fieldName: "YEAR", label: "Last Year Counted", visible: true, format: { places: 0} },
-        { fieldName: "RouteName", label: "Route Name", visible: true, format: { places: 0} },
-        { fieldName: "RouteNum", label: "Route Number", visible: true, format: { places: 0} },
-      ],
-    });
+    // //AADT Template--------------------
+    // var aadtTemplate = new PopupTemplate({
+    //   title: "Traffic Data",
+    //
+    //   fieldInfos: [
+    //     { fieldName: "aadt", label: "AADT", visible: true, format: { places: 0} },
+    //     { fieldName: "ATRStation", label: "Automated Traffic Recording Station", visible: true, format: { places: 0} },
+    //     { fieldName: "YEAR", label: "Last Year Counted", visible: true, format: { places: 0} },
+    //     { fieldName: "RouteName", label: "Route Name", visible: true, format: { places: 0} },
+    //     { fieldName: "RouteNum", label: "Route Number", visible: true, format: { places: 0} },
+    //   ],
+    // });
 
 
 
@@ -201,46 +209,46 @@ require([
       outFields: ["*"],
     });
 
-    // --------------------------------------------------
-    //Create AADT Line Feature Layer--------------------------------
-    // --------------------------------------------------
-    var aadtUrl = "https://services1.arcgis.com/NXmBVyW5TaiCXqFs/ArcGIS/rest/services/AADT_2013_StateHighways/FeatureServer/0";
-
-    var aadtLine = new FeatureLayer(aadtUrl, {
-      mode: FeatureLayer.MODE_AUTO,
-      outFields: ["*"],
-      infoTemplate: aadtTemplate,
-      minScale: 288000,
-    });
-
-    var aadtSymbol = new CartographicLineSymbol(    );
-    aadtSymbol.style = CartographicLineSymbol.STYLE_DASH;
-    aadtSymbol.setCap("ROUND");
-    aadtSymbol.setJoin("ROUND");
-    aadtSymbol.setColor([230, 0, 169, 1]);
-
-    //Create a unique value renderer and its unique value info
-    var renderer = new UniqueValueRenderer(aadtSymbol);
-
-    /**********************************************
-    * Define a size visual variable to vary the width
-    * of each highway based on its annual average daily
-    * traffic count.
-    *********************************************/
-    renderer.setVisualVariables([{
-        type: "sizeInfo",
-        field: "aadt",
-        valueUnit: "unknown",
-        minSize: 2.5,
-        maxSize: 10,
-        minDataValue: 10,
-        maxDataValue: 56000
-    }]);
-
-    //Set the renderer on the layer and add the layer to the map
-    aadtLine.setRenderer(renderer);
-    // --------------------------------------------------
-    // --------------------------------------------------
+    // // --------------------------------------------------
+    // //Create AADT Line Feature Layer--------------------------------
+    // // --------------------------------------------------
+    // var aadtUrl = "https://services1.arcgis.com/NXmBVyW5TaiCXqFs/ArcGIS/rest/services/AADT_2013_StateHighways/FeatureServer/0";
+    //
+    // var aadtLine = new FeatureLayer(aadtUrl, {
+    //   mode: FeatureLayer.MODE_AUTO,
+    //   outFields: ["*"],
+    //   infoTemplate: aadtTemplate,
+    //   minScale: 288000,
+    // });
+    //
+    // var aadtSymbol = new CartographicLineSymbol(    );
+    // aadtSymbol.style = CartographicLineSymbol.STYLE_DASH;
+    // aadtSymbol.setCap("ROUND");
+    // aadtSymbol.setJoin("ROUND");
+    // aadtSymbol.setColor([230, 0, 169, 1]);
+    //
+    // //Create a unique value renderer and its unique value info
+    // var renderer = new UniqueValueRenderer(aadtSymbol);
+    //
+    // /**********************************************
+    // * Define a size visual variable to vary the width
+    // * of each highway based on its annual average daily
+    // * traffic count.
+    // *********************************************/
+    // renderer.setVisualVariables([{
+    //     type: "sizeInfo",
+    //     field: "aadt",
+    //     valueUnit: "unknown",
+    //     minSize: 2.5,
+    //     maxSize: 10,
+    //     minDataValue: 10,
+    //     maxDataValue: 56000
+    // }]);
+    //
+    // //Set the renderer on the layer and add the layer to the map
+    // aadtLine.setRenderer(renderer);
+    // // --------------------------------------------------
+    // // --------------------------------------------------
 
 
 
@@ -273,7 +281,7 @@ require([
 
 
     //Add Layers to Map
-    map.addLayer(aadtLine);
+    // map.addLayer(aadtLine);
     map.addLayer(railLine);
     map.addLayer(milePostsTen);
     map.addLayer(milePostsFive);
@@ -284,27 +292,27 @@ require([
 
 
 
-//-------------------------------------------------------------------------
-//-----------------------LayerToggle--------------------------------
-//-------------------------------------------------------------------------
-    var toggleLayers = [
-      {
-        layer: signPoints,
-        content: "<b>Signs</b> <img src='img/favicon.png' alt='site image' height=17px style='border-radius:4px; float:right; right:5px'>",
-      },
-      {
-        layer: aadtLine,
-        content: "<b>AADT</b> <img src='img/aadt.png' alt='site image' width=60px style='border-radius:4px; float:right; right:5px'>",
-      },
-    ];
-
-    var myLayerList = new LayerList({
-      map: map,
-      layers: toggleLayers,
-      theme: "vtransTheme",
-    }, "layerList");
-    myLayerList.startup();
-//-------------------------------------------------------------------------
+// //-------------------------------------------------------------------------
+// //-----------------------LayerToggle--------------------------------
+// //-------------------------------------------------------------------------
+//     var toggleLayers = [
+//       {
+//         layer: signPoints,
+//         content: "<b>Signs</b> <img src='img/favicon.png' alt='site image' height=17px style='border-radius:4px; float:right; right:5px'>",
+//       },
+//       {
+//         layer: aadtLine,
+//         content: "<b>AADT</b> <img src='img/aadt.png' alt='site image' width=60px style='border-radius:4px; float:right; right:5px'>",
+//       },
+//     ];
+//
+//     var myLayerList = new LayerList({
+//       map: map,
+//       layers: toggleLayers,
+//       theme: "vtransTheme",
+//     }, "layerList");
+//     myLayerList.startup();
+// //-------------------------------------------------------------------------
 
 
 
