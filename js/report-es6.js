22fb22213b8e4c556cc65d5bd02bff5553e4231a
jfarmer91 <farmerjong@gmail.com>
2016-01-04 11:22:20 -0500

    Part of Issue #184: Report Images are links to open full image in new tab

--- js/report-es6.js
+++ js/report-es6.js
@@ -82,7 +82,7 @@ require([
           for ( i = 0; i < response.length; i++) {
             imgSrc = response[i].url;
             // imageString += "<div class='col-sm-6 col-md-4'><img src='" + imgSrc + "' " + imgClass + " " + imageStyle + "></div>";
-            imageString += "<div data-field-span='1'><img src='" + imgSrc + "' " + imgClass + " " + imageStyle + "></div>";
+            imageString += "<div data-field-span='1'><a href='" + imgSrc + "' target='_blank'><img src='" + imgSrc + "' " + imgClass + " " + imageStyle + "></a></div>";
           }
         }
 
