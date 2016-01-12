e45d89f5343bf2a304c8364e517f4320ce8c2797
jfarmer91 <farmerjong@gmail.com>
2016-01-12 12:21:21 -0500

    Part of Issue #207: update output image file name
    - update python image scrape script to make ATT_NAME at the start of the
    output image file name

--- imgProcessing/ExportAttachments.py
+++ imgProcessing/ExportAttachments.py
@@ -8,8 +8,8 @@ fileLocation = arcpy.GetParameterAsText(1)
 with da.SearchCursor(inTable, ['DATA', 'ATT_NAME', 'ATTACHMENTID', 'DOT_Num', 'SignUID']) as cursor:
     for item in cursor:
         attachment = item[0]
-        filenum = str(item[3]) + "_ATT" + str(item[2]) + "_"
-        filename = filenum + str(item[1])
+        filenum = str(item[1])[0:6] + "_" + str(item[3]) + "_ATT" + str(item[2])
+        filename = filenum + str(item[1])[6:10]
         if not os.path.exists(fileLocation + "/" + str(item[3]) + "-" + str(item[4])):
             os.makedirs(fileLocation + "/" + str(item[3]) + "-" + str(item[4]))
         open(fileLocation + "/" + str(item[3]) + "-" + str(item[4]) + os.sep + filename, 'wb').write(attachment.tobytes())
