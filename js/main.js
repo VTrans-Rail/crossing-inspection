9cbdd46a7ae062575dec6404dcc72a776dc53737
jfarmer91 <farmerjong@gmail.com>
2016-01-14 16:54:24 -0500

    Part of Issue #202: for modern browsers
    - started with testing for IE

--- js/main.js
+++ js/main.js
@@ -1,5 +1,14 @@
 var formatString = "";
 
+var isOpera = !!window.opera || navigator.userAgent.indexOf(' OPR/') >= 0;
+    // Opera 8.0+ (UA detection to detect Blink/v8-powered Opera)
+var isFirefox = typeof InstallTrigger !== 'undefined';   // Firefox 1.0+
+var isSafari = Object.prototype.toString.call(window.HTMLElement).indexOf('Constructor') > 0;
+    // At least Safari 3+: "[object HTMLElementConstructor]"
+var isChrome = !!window.chrome && !isOpera;              // Chrome 1+
+var isIE = /*@cc_on!@*/false || !!document.documentMode;   // At least IE6
+
+
 require([
   "esri/map",
   "esri/arcgis/utils",
@@ -583,5 +592,10 @@ require([
 
     //Finalize creation of the search widget
     searchWidget.startup();
+
+    if ( isIE ) {
+      alert("Crossing Inspection Web Application designed to be viewed in modern browsers. For best results, open in Chrome or Firefox.");
+    }
+
 
 });
--- js/report.js
+++ js/report.js
@@ -6,6 +6,14 @@ function getParameterByName(name) {
   return results == null ? "" : decodeURIComponent(results[1].replace(/\+/g, " "));
 }
 
+var isOpera = !!window.opera || navigator.userAgent.indexOf(' OPR/') >= 0;
+    // Opera 8.0+ (UA detection to detect Blink/v8-powered Opera)
+var isFirefox = typeof InstallTrigger !== 'undefined';   // Firefox 1.0+
+var isSafari = Object.prototype.toString.call(window.HTMLElement).indexOf('Constructor') > 0;
+    // At least Safari 3+: "[object HTMLElementConstructor]"
+var isChrome = !!window.chrome && !isOpera;              // Chrome 1+
+var isIE = /*@cc_on!@*/false || !!document.documentMode;   // At least IE6
+
 
 require([
   "dojo/dom", "dojo/on",
@@ -121,6 +129,11 @@ if (dotnumqs) {
             queryTask.execute(query,showResults);
           }
       }
+
+      if ( isIE ) {
+        alert("Crossing Inspection Web Application designed to be viewed in modern browsers. For best results, open in Chrome or Firefox.");
+      }
+
 
       function showResults (results) {
         var resultItems = [];
