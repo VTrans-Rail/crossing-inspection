666c072aa271b65c80ea8ed05f4d897e8d9c86ee
jfarmer91 <farmerjong@gmail.com>
2016-01-27 15:39:53 -0500

    Part of issue #250: switch to onclick events

--- js/photo.js
+++ js/photo.js
@@ -13,86 +13,139 @@ function getParameterByName(name) {
 
 var photoUrl = getParameterByName("url");
 
-document.getElementById("info").innerHTML = "<img src='" + photoUrl + "' id='image' alt='site-image' width='100%'>";
+document.getElementById("info").innerHTML = "<img src='" + photoUrl + "' id='image' alt='site-image' width='100%' onclick='zoom(this)'>";
 
-var pictureDiv = document.getElementById("info");
 
-var hammertime = new Hammer(pictureDiv);
 
-hammertime.get('pan').set({ direction: Hammer.DIRECTION_ALL });
+function zoom (x) {
+  var previousWidth = document.getElementById("save-old-width").innerHTML;
 
-hammertime.get('pinch').set({ enable: true});
+  console.log(previousWidth);
+  var width = x.width;
+  document.getElementById("save-old-width").innerHTML = width;
+  console.log(previousWidth);
+  console.log(width);
 
-hammertime.on('pinch panleft panright panup pandown tap press', function(ev) {
-  console.log(ev.type + "whatever");
-  if (ev.type === "pinch") {
-    MouseWheelHandler();
-    alert("wow");
+  // if ( previousWidth === "" ) {
+  //   x.style.width = Math(x.style.width + 750);
+  // } else if ( previousWidth < width ) {
+  //   if ( width < 3000 ) {
+  //     x.style.width = x.style.width = x.style.width + "750px";
+  //   } else {
+  //     x.style.width = x.style.width = x.style.width - "750px";
+  //   }
+  // } else if ( previousWidth === width ) {
+  //
+  // } else if ( previousWidth > width ) {}
+
+  var widthRatio = ( width / previousWidth );
+  console.log(widthRatio);
+
+
+  // if ( x.width > 3199 ) {
+  //   console.log("eh");
+  //   x.style.width = "100%";
+  // } else {
+  //   x.style.width = "3200px";
+  //   // console.log(x.style.width);
+  // }
+  // var widthRatio = ( width / previousWidth );
+  // console.log(widthRatio);
+  //
+  if ( previousWidth === "" ) {
+    x.style.width = "200%";
+  } else if ( widthRatio === 2 ) {
+    x.style.width = "300%";
+  } else if ( widthRatio === 1.5 ) {
+    console.log("eh");
+    x.style.width = "200%";
+  } else if ( widthRatio === (2/3) ) {
+    x.style.width = "100%";
+  } else if (widthRatio === 0.5 ) {
+    x.style.width = "200%";
   }
-});
 
-var image = document.getElementById("image");
-
-if (image.addEventListener) {
-	// IE9, Chrome, Safari, Opera
-	image.addEventListener("mousewheel", MouseWheelHandler, false);
-	// Firefox
-	image.addEventListener("DOMMouseScroll", MouseWheelHandler, false);
 }
 
-function MouseWheelHandler() {
-  var e = window.event || e; // old IE support
-  var delta = Math.max(-1, Math.min(1, (e.wheelDelta || -e.detail)));
+// var pictureDiv = document.getElementById("info");
+//
+//
+// var hammertime = new Hammer(pictureDiv);
+//
+// hammertime.get('pan').set({ direction: Hammer.DIRECTION_ALL });
+//
+// hammertime.get('pinch').set({ enable: true});
+//
+// hammertime.on('pinch pinchin pinchout panleft panright panup pandown tap press', function(ev) {
+//   console.log(ev.type + "whatever");
+//   if (ev.type === "pinch") {
+//     MouseWheelHandler();
+//     alert("wow");
+//   }
+// });
 
-  image.style.width = Math.max(200, Math.min(4320, image.width + (100 * delta))) + "px";
+// var image = document.getElementById("image");
+//
+// if (image.addEventListener) {
+// 	// IE9, Chrome, Safari, Opera
+// 	image.addEventListener("mousewheel", MouseWheelHandler, false);
+// 	// Firefox
+// 	image.addEventListener("DOMMouseScroll", MouseWheelHandler, false);
+// }
+//
+// function MouseWheelHandler() {
+//   var e = window.event || e; // old IE support
+//   var delta = Math.max(-1, Math.min(1, (e.wheelDelta || -e.detail)));
+//
+//   image.style.width = Math.max(200, Math.min(4320, image.width + (100 * delta))) + "px";
+//
+//   return false;
+// }
 
-  return false;
-}
 
-
-  (function($){
-
-    $(document).ready(function(){
-      //call imagePanning fn when DOM is ready
-      $(".content img").imagePanning();
-    });
-
-    //imagePanning fn
-    $.fn.imagePanning=function(){
-        var init="center";
-        return this.each(function(){
-          var $this=$(this);
-          if($this.data("imagePanning")) return;
-          $this.data("imagePanning",1)
-            //create markup
-            .wrap("<div class='img-pan-container' />")
-            .after("<div class='resize' style='position:absolute; width:auto; height:auto; top:0; right:0; bottom:0; left:0; margin:0; padding:0; overflow:hidden; visibility:hidden; z-index:-1'><iframe style='width:100%; height:0; border:0; visibility:visible; margin:0' /><iframe style='width:0; height:100%; border:0; visibility:visible; margin:0' /></div>")
-            //image loaded fn
-            .one("load",function(){
-              setTimeout(function(){ $this.addClass("loaded").trigger("mousemove",1); },1);
-            }).each(function(){ //run load fn even if cached
-              if(this.complete) $(this).load();
-            })
-            //panning fn
-            .parent().on("mousemove touchmove MSPointerMove pointermove",function(e,p){
-              var cont=$(this);
-              e.preventDefault();
-              var contH=cont.height(),contW=cont.width(),
-                isTouch=e.type.indexOf("touch")!==-1,isPointer=e.type.indexOf("pointer")!==-1,
-                evt=isPointer ? e.originalEvent : isTouch ? e.originalEvent.touches[0] || e.originalEvent.changedTouches[0] : e,
-                coords=[
-                  !p ? evt.pageY-cont.offset().top : init==="center" ? contH/2 : 0,
-                  !p ? evt.pageX-cont.offset().left : init==="center" ? contW/2 : 0
-                ],
-                dest=[Math.round(($this.outerHeight(true)-contH)*(coords[0]/contH)),Math.round(($this.outerWidth(true)-contW)*(coords[1]/contW))];
-              $this.css({"top":-dest[0],"left":-dest[1]});
-            })
-            //resize fn
-            .find(".resize iframe").each(function(){
-              $(this.contentWindow || this).on("resize",function(){
-                $this.trigger("mousemove",1);
-              });
-            });
-        });
-      }
-  })(jQuery);
+  // (function($){
+  //
+  //   $(document).ready(function(){
+  //     //call imagePanning fn when DOM is ready
+  //     $(".content img").imagePanning();
+  //   });
+  //
+  //   //imagePanning fn
+  //   $.fn.imagePanning=function(){
+  //       var init="center";
+  //       return this.each(function(){
+  //         var $this=$(this);
+  //         if($this.data("imagePanning")) return;
+  //         $this.data("imagePanning",1)
+  //           //create markup
+  //           .wrap("<div class='img-pan-container' />")
+  //           .after("<div class='resize' style='position:absolute; width:auto; height:auto; top:0; right:0; bottom:0; left:0; margin:0; padding:0; overflow:hidden; visibility:hidden; z-index:-1'><iframe style='width:100%; height:0; border:0; visibility:visible; margin:0' /><iframe style='width:0; height:100%; border:0; visibility:visible; margin:0' /></div>")
+  //           //image loaded fn
+  //           .one("load",function(){
+  //             setTimeout(function(){ $this.addClass("loaded").trigger("mousemove",1); },1);
+  //           }).each(function(){ //run load fn even if cached
+  //             if(this.complete) $(this).load();
+  //           })
+  //           //panning fn
+  //           .parent().on("mousemove touchmove MSPointerMove pointermove",function(e,p){
+  //             var cont=$(this);
+  //             e.preventDefault();
+  //             var contH=cont.height(),contW=cont.width(),
+  //               isTouch=e.type.indexOf("touch")!==-1,isPointer=e.type.indexOf("pointer")!==-1,
+  //               evt=isPointer ? e.originalEvent : isTouch ? e.originalEvent.touches[0] || e.originalEvent.changedTouches[0] : e,
+  //               coords=[
+  //                 !p ? evt.pageY-cont.offset().top : init==="center" ? contH/2 : 0,
+  //                 !p ? evt.pageX-cont.offset().left : init==="center" ? contW/2 : 0
+  //               ],
+  //               dest=[Math.round(($this.outerHeight(true)-contH)*(coords[0]/contH)),Math.round(($this.outerWidth(true)-contW)*(coords[1]/contW))];
+  //             $this.css({"top":-dest[0],"left":-dest[1]});
+  //           })
+  //           //resize fn
+  //           .find(".resize iframe").each(function(){
+  //             $(this.contentWindow || this).on("resize",function(){
+  //               $this.trigger("mousemove",1);
+  //             });
+  //           });
+  //       });
+  //     }
+  // })(jQuery);
